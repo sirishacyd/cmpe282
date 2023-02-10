66f0a8818243b3ab2cb54f867ec439b420210bbe
sirisha <103618216+sirishacyd@users.noreply.github.com>
2023-02-09 20:25:46 -0800

    Update sentiment_analysis.py

--- sentiment_analysis.py
+++ sentiment_analysis.py
@@ -1,1 +1,11 @@
 
+from textblob import TextBlob
+
+def sentiment_analysis(text):
+    analysis = TextBlob(text)
+    if analysis.sentiment.polarity > 0:
+        return "Positive"
+    elif analysis.sentiment.polarity == 0:
+        return "Neutral"
+    else:
+        return "Negative"
